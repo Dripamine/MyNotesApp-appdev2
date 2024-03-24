74ae26b051ed34ef831c21ebc954d8a98da1afc8
Bo Kyung Kim <bkkim022@gmail.com>
2024-03-23 22:49:46 -0400

    updated login and registration styles

--- XingBranch/Notesapp/Controllers/UserController.cs
+++ XingBranch/Notesapp/Controllers/UserController.cs
@@ -1,39 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity.Infrastructure;
-using System.Linq;
-using System.Web;
+﻿using System.Linq;
 using System.Web.Mvc;
 using BCrypt.Net;
-
 
 namespace Notesapp.Controllers
 {
     public class UserController : Controller
     {
-        // GET: User
-        public ActionResult AddOrEdit(int id=0)
+        [HttpGet]
+        public ActionResult AddOrEdit(int id = 0)
         {
             User userModel = new User();
             return View(userModel);
         }
+
         [HttpPost]
         public ActionResult AddOrEdit(User userModel)
         {
-            using (appdev2Entities dbcontext = new appdev2Entities())
+            if (ModelState.IsValid)
             {
-                if (dbcontext.Users.Any(x => x.Username == userModel.Username))
+                if (userModel.Password != userModel.ConfirmPassword)
                 {
-                    ViewBag.DuplicateMessage = "Username already exists.";
+                    ModelState.AddModelError("ConfirmPassword", "The password and confirmation password do not match.");
                     return View("AddOrEdit", userModel);
                 }
-                userModel.Password = BCrypt.Net.BCrypt.HashPassword(userModel.Password);
-                dbcontext.Users.Add(userModel);
-                dbcontext.SaveChanges();
+
+                using (appdev2Entities dbcontext = new appdev2Entities())
+                {
+                    if (dbcontext.Users.Any(x => x.Username == userModel.Username))
+                    {
+                        ViewBag.DuplicateMessage = "Username already exists.";
+                        return View("AddOrEdit", userModel);
+                    }
+                    userModel.Password = BCrypt.Net.BCrypt.HashPassword(userModel.Password);
+                    dbcontext.Users.Add(userModel);
+                    dbcontext.SaveChanges();
+                }
+                ModelState.Clear();
+                ViewBag.SuccessMessage = "Registration Successful";
+                return View("AddOrEdit", new User());
             }
-            ModelState.Clear();
-            ViewBag.SuccessMessage = "Registration Successful";
-            return View("AddOrEdit", new User());
+            return View("AddOrEdit", userModel);
         }
     }
 }
