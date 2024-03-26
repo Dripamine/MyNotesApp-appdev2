b0ac220045094d695846fb4f0399c58bd4501773
xingr121 <xinghuang121@gmail.com>
2024-03-26 14:22:59 -0400

    create notes validation

--- XingBranch/Notesapp/Note.cs
+++ XingBranch/Notesapp/Note.cs
@@ -11,11 +11,15 @@ namespace Notesapp
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class Note
     {
         public int NoteId { get; set; }
+
+        [Required (ErrorMessage = "Title is required")]
         public string Title { get; set; }
+        [Required(ErrorMessage = "Content is required")]
         public string Content { get; set; }
         public System.DateTime Date { get; set; }
         public Nullable<int> UserId { get; set; }
